a2c6c2a44e634d05cb307a2178e905e6b02b6476
paike <github@pradikow.de>
2024-07-20 07:56:04 +0200

    added logging. config can be modified by env

--- backend.py
+++ backend.py
@@ -4,17 +4,42 @@ import langroid as lr
 import langroid.language_models as lm
 import os
 
-app = Flask(__name__)
+import logging
 
+
+app = Flask(__name__)
+logger = app.logger
+logger.setLevel(logging.INFO)
 Role = lr.language_models.Role
 LLMMessage = lr.language_models.LLMMessage
 # config = lr.language_models.OpenAIGPTConfig(
 #     chat_model=lr.language_models.OpenAIChatModel.GPT4,
 # )
+
+LLM_API_URL = os.getenv("LLM_API_URL", "127.0.0.1")
+LLM_API_PORT = os.getenv("LLM_API_PORT", 8000)
+LLM_SYSTEM_MESSAGE = os.getenv(
+    "LLM_SYSTEM_MESSAGE", "You are a helpful assistant")
+LLM_MAX_TOKENS = os.getenv("LLM_MAX_TOKENS", 300)
+DEBUG = os.getenv('DEBUG', False)
+FLASK_PORT = os.getenv('FLASK_PORT', 5000)
+
+logger = app.logger
+print(logger.level)
+logger.setLevel(logging.DEBUG) if DEBUG else logger.setLevel(logging.INFO)
+Role = lr.language_models.Role
+LLMMessage = lr.language_models.LLMMessage
+
+logger.info(f"LLM_API_URL: {LLM_API_URL}")
+logger.info(f"LLM_API_PORT: {LLM_API_PORT}")
+logger.info(f"FLASK_PORT: {FLASK_PORT}")
+logger.info(f"LLM_SYSTEM_MESSAGE: {LLM_SYSTEM_MESSAGE}")
+logger.info(f"LLM_SYSTEM_MESSAGE: {LLM_MAX_TOKENS}")
+
 config = lm.OpenAIGPTConfig(
-    
-    api_base=os.getenv('LLM_API_URL')
-    )
+    api_base=f"{LLM_API_URL}:{LLM_API_PORT}"
+)
+
 model = lr.language_models.OpenAIGPT(config)
 
 # config = lr.ChatAgentConfig(
@@ -22,22 +47,21 @@ model = lr.language_models.OpenAIGPT(config)
 #     llm = lm.OpenAIGPTConfig(
 #         api_base=os.getenv('LLM_API_URL')
 #     ),
-#     use_tools=False, 
-#     use_functions_api=False, 
+#     use_tools=False,
+#     use_functions_api=False,
 #     vecdb=None,
 # )
 # agent = lr.ChatAgent(config=config)
 
 
-# task = lr.Task(agent, 
-# name="Assistant", 
-# system_message="you are a dog and the mans best friend.", 
+# task = lr.Task(agent,
+# name="Assistant",
+# system_message="you are a dog and the mans best friend.",
 # interactive=False,
 # )
 
 # print(task.run())
 
-print(os.getenv('LLM_API_URL'))
 
 def get_messages_from_request(data):
     assistant = 'assistant'
@@ -65,31 +89,36 @@ def get_messages_from_request(data):
     return messages
 
 
-
-
-# @app.route('/api/data', methods=['GET'])
-# def get_data():
-#     data = {'message': 'Hello from Flask'}
-#     return jsonify(data)
-
-# standard messaging route
+@app.route('/api/healthcheck', methods=['GET'])
+def healthcheck():
+    # Perform your health checks here (e.g., database, external services)
+    # For simplicity, we'll just return a static response
+    health_status = {
+        "status": "OK",
+        "uptime": "100%",
+        "dependencies": {
+            "inference_server": "OK"
+        }
+    }
+    logger.debug(f"Requested health check: {health_status}")
+    return jsonify(health_status), 200
 
 
 @app.route('/api/inference', methods=['POST'])
 def post_data():
     content = request.json
-
+    logger.debug(f"Request: {request}")
     messages = []
     messages.append(
-        LLMMessage(content=os.getenv('LLM_SYSTEM_MESSAGE'), role=Role.SYSTEM), 
+        LLMMessage(content=LLM_SYSTEM_MESSAGE, role=Role.SYSTEM),
     )
 
     messages.extend(get_messages_from_request(content))
-
-    response = model.chat(messages=messages, max_tokens=200)
-
+    logger.debug(f"Messages: {messages}")
+    response = model.chat(messages=messages, max_tokens=LLM_MAX_TOKENS)
+    logger.debug(f"LLM Response: {response}")
     return jsonify(response.message.replace('<|eot_id|>', ''))
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv('FLASK_PORT'))
+    app.run(debug=DEBUG, port=FLASK_PORT)
