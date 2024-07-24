3ec09e5845ca91ced5553d1913d3180604652318
paike <github@pradikow.de>
2024-07-24 13:37:22 +0200

    added option to inject context from context file

--- backend.py
+++ backend.py
@@ -26,9 +26,11 @@ LLM_API_KEY = os.getenv("LLM_API_KEY", "nokey")
 LLM_API_MODEL = os.getenv("LLM_API_MODEL", "gpt-4o-mini")
 LLM_API_URL = os.getenv("LLM_API_URL", f"{LLM_API_SCHEME}://{LLM_API_HOST}:{LLM_API_PORT}")
 
+CONTEXT_FILEPATH = os.getenv("CONTEXT_FILEPATH", "context/context.md")
+
 LLM_SYSTEM_MESSAGE = os.getenv("LLM_SYSTEM_MESSAGE", "You are a helpful assistant.")
 
-LLM_MAX_TOKENS = os.getenv("LLM_MAX_TOKENS", 300)
+LLM_MAX_TOKENS = int(os.getenv("LLM_MAX_TOKENS", 300))
 DEBUG = os.getenv('DEBUG', False)
 FLASK_PORT = os.getenv('BACKEND_API_PORT', 5000)
 
@@ -45,37 +47,33 @@ logger.debug(f"LLM_API_PORT: {LLM_API_PORT}")
 logger.info(f"LLM_API_URL: {LLM_API_URL}")
 logger.info(f"LLM_API_MODEL: {LLM_API_MODEL}")
 logger.info(f"BACKEND_API_PORT: {FLASK_PORT}")
+logger.debug(f"CONTEXT_FILEPATH: {CONTEXT_FILEPATH}")
 logger.info(f"LLM_SYSTEM_MESSAGE: {LLM_SYSTEM_MESSAGE}")
 logger.info(f"LLM_SYSTEM_MESSAGE: {LLM_MAX_TOKENS}")
 
 config = lm.OpenAIGPTConfig(
     api_base=LLM_API_URL,
     api_key=LLM_API_KEY,
-    chat_model=LLM_API_MODEL
+    chat_model=LLM_API_MODEL,
+    max_output_tokens=LLM_MAX_TOKENS
 )
 
 model = lm.OpenAIGPT(config)
 
-# config = lr.ChatAgentConfig(
-#     name="Assistant",
-#     llm = lm.OpenAIGPTConfig(
-#         api_base=os.getenv('LLM_API_HOST')
-#     ),
-#     use_tools=False,
-#     use_functions_api=False,
-#     vecdb=None,
-# )
-# agent = lr.ChatAgent(config=config)
+def get_system_message():
 
-
-# task = lr.Task(agent,
-# name="Assistant",
-# system_message="you are a dog and the mans best friend.",
-# interactive=False,
-# )
-
-# print(task.run())
-
+    try:
+        with open(CONTEXT_FILEPATH, 'r') as file:
+            context = file.read()
+        system_message = LLM_SYSTEM_MESSAGE.replace('{context}', context)
+        logger.debug(f"Injecting context")
+    except FileNotFoundError:
+        # Handle the case where the file does not exist
+        context = ""  # Or any default context you want to use
+        system_message = LLM_SYSTEM_MESSAGE.replace('{context}', context)
+        # logger.debug(f"System message: {system_message}")
+        logger.debug(f"There is no context file")
+    return system_message
 
 def get_messages_from_request(data):
     assistant = 'assistant'
@@ -90,6 +88,7 @@ def get_messages_from_request(data):
                 timestamp=message['timestamp']
             )
         )
+
 
     last_message = data['lastMessage']
     logger.debug(f"Last message: {last_message}")
@@ -125,11 +124,11 @@ def post_data():
     content = request.json
     logger.debug(f"Request: {request}")
     messages = []
-    current_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S (%A)')
-    system_message = LLM_SYSTEM_MESSAGE + f" The current date and time is: {current_time}."
-    logger.debug(f"Current system message: {system_message}")
+    # current_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S (%A)')
+    # system_message = LLM_SYSTEM_MESSAGE + f" The current date and time is: {current_time}."
+    # logger.debug(f"Current system message: {system_message}")
     messages.append(
-        LLMMessage(content=system_message, role=Role.SYSTEM),
+        LLMMessage(content=get_system_message(), role=Role.SYSTEM),
     )
     system_message = ""
     messages.extend(get_messages_from_request(content))
